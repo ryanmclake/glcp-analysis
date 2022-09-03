2812b1248401354b6c2856ba8ebcd42476c7848d
ryanmclake <ryan333@vt.edu>
2022-09-03 09:30:24 -0400

    expand ecoregion glcp linkages. most things seem to check out.

--- code/kamiak/Q2_Ecoregion_link_GLCP.R
+++ code/kamiak/Q2_Ecoregion_link_GLCP.R
@@ -9,7 +9,57 @@ library(units)
 
 #### I AM STILL IN PROGRESS #####
 
-shp_boreal <- read_sf(paste0("./data/shapes/WWF_ecoregions/official/wwf_terr_ecos.shp")) %>%
+Dir.Base <- getwd()
+Dir.Data <- file.path(Dir.Base, "data")
+Dir.Shapes <- file.path(Dir.Data, "shapes")
+
+if (!file.exists(file.path(Dir.Shapes, "WWF_ecoregions"))) {
+  download.file("http://assets.worldwildlife.org/publications/15/files/original/official_teow.zip",
+                destfile = file.path(Dir.Shapes, "wwf_ecoregions.zip"))
+  unzip(file.path(Dir.Shapes, "wwf_ecoregions.zip"), exdir = file.path(Dir.Shapes, "WWF_ecoregions"))
+}
+
+EcoregionMask <- read_sf(file.path(Dir.Base,"data","shapes", "WWF_ecoregions", "official", "wwf_terr_ecos.shp"))
+EcoregionMask <- st_make_valid(EcoregionMask)
+
+EcoregionMask_hex <- st_make_valid(EcoregionMask)%>%
+  st_transform("+proj=eqearth +wktext")
+
+
+# link to significant increasing sens_slopes
+
+slope_data_swell <- vroom::vroom("./output/hylak_id_kendall_sens.csv", delim = " ", col_names = T) %>%
+  mutate_all(funs(as.numeric(.))) %>%
+  filter(!(hylak_id %% 1)) %>%
+  na.omit(.) %>%
+  filter(p.value < 0.050000000000) %>%
+  filter(sens.slope > 0.0000000000) %>%
+  st_as_sf(coords = c("centr_lon", "centr_lat"), crs = 4326) %>%
+  st_transform("+proj=eqearth +wktext")
+
+dam_data <- vroom::vroom("./data/GeoDAR_v11_dams_beta_pr1.csv", delim = ",", col_names = T) %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326) %>%
+  st_transform("+proj=eqearth +wktext")
+
+dam_glcp_link <- dam_data %>%
+  cbind(slope_data_swell[st_nearest_feature(dam_data, slope_data_swell),]) %>%
+  mutate(dist = st_distance(geometry, geometry.1, by_element = T))
+#
+reservoirs <- c(unique(dam_glcp_link$hylak_id))
+
+`%!in%` <- Negate(`%in%`)
+
+slope_data_swell_2 <- slope_data_swell %>%
+  filter(hylak_id %!in% reservoirs)
+
+slope_data_swell_2 <- slope_data_swell_2[!duplicated(slope_data_swell_2$hylak_id), ]
+
+# takes a while! Sit tight. 
+link <- slope_data_swell_2 %>%
+  cbind(EcoregionMask_hex[st_nearest_feature(slope_data_swell_2, EcoregionMask_hex),]) %>%
+  mutate(dist = st_distance(geometry, geometry.1, by_element = T))
+
+lakes_boreal_swell <- link %>%
   mutate(biome_type = case_when(
     BIOME == 1 ~ "TROPICAL MOIST FOREST",
     BIOME == 2 ~ "TROPICAL DRY FOREST",
@@ -31,7 +81,10 @@ shp_boreal <- read_sf(paste0("./data/shapes/WWF_ecoregions/official/wwf_terr_eco
   st_transform("+proj=eqearth +wktext") %>%
   filter(biome_type %in% c("BOREAL FOREST","ROCK & ICE","TUNDRA"))
 
-shp_desert <- read_sf(paste0("./data/shapes/WWF_ecoregions/official/wwf_terr_ecos.shp")) %>%
+
+lakes_boreal_swell <- c(unique(lakes_boreal_swell$hylak_id))
+
+shp_desert_swell <- link %>%
   mutate(biome_type = case_when(
     BIOME == 1 ~ "TROPICAL MOIST FOREST",
     BIOME == 2 ~ "TROPICAL DRY FOREST",
@@ -53,8 +106,9 @@ shp_desert <- read_sf(paste0("./data/shapes/WWF_ecoregions/official/wwf_terr_eco
   st_transform("+proj=eqearth +wktext") %>%
   filter(biome_type == "DESERT")
 
+shp_desert_swell <- c(unique(shp_desert_swell$hylak_id))
 
-shp_temperate <- read_sf(paste0("./data/shapes/WWF_ecoregions/official/wwf_terr_ecos.shp")) %>%
+shp_temperate_swell <- link %>%
   mutate(biome_type = case_when(
     BIOME == 1 ~ "TROPICAL MOIST FOREST",
     BIOME == 2 ~ "TROPICAL DRY FOREST",
@@ -77,7 +131,9 @@ shp_temperate <- read_sf(paste0("./data/shapes/WWF_ecoregions/official/wwf_terr_
   filter(biome_type %in% c("TEMPERATE GRASSLAND","TEMPERATE CONIFEROUS FOREST","MEDITERRANIAN FOREST","FLOODED GRASSLAND",
                            "MONTANE GRASSLAND","LAKE","TEMPERATE BROADLEAF FOREST"))
 
-shp_tropical <- read_sf(paste0("./data/shapes/WWF_ecoregions/official/wwf_terr_ecos.shp")) %>%
+shp_temperate_swell <- c(unique(shp_temperate_swell$hylak_id))
+
+shp_tropical_swell <- link %>%
   mutate(biome_type = case_when(
     BIOME == 1 ~ "TROPICAL MOIST FOREST",
     BIOME == 2 ~ "TROPICAL DRY FOREST",
@@ -99,3 +155,146 @@ shp_tropical <- read_sf(paste0("./data/shapes/WWF_ecoregions/official/wwf_terr_e
   st_transform("+proj=eqearth +wktext") %>%
   filter(biome_type %in% c("TROPICAL MOIST FOREST","TROPICAL DRY FOREST","TROPICAL GRASSLAND","MANGROVES",
                            "TROPICAL CONIFEROUS FOREST"))
+
+shp_tropical_swell <- c(unique(shp_tropical_swell$hylak_id))
+
+
+
+
+
+###################################################################################################################
+
+
+
+
+# link to significant decreasing sens_slopes
+
+slope_data_shrink <- vroom::vroom("./output/hylak_id_kendall_sens.csv", delim = " ", col_names = T) %>%
+  mutate_all(funs(as.numeric(.))) %>%
+  filter(!(hylak_id %% 1)) %>%
+  na.omit(.) %>%
+  filter(p.value < 0.050000000000) %>%
+  filter(sens.slope < 0.0000000000) %>%
+  st_as_sf(coords = c("centr_lon", "centr_lat"), crs = 4326) %>%
+  st_transform("+proj=eqearth +wktext")
+
+dam_data <- vroom::vroom("./data/GeoDAR_v11_dams_beta_pr1.csv", delim = ",", col_names = T) %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326) %>%
+  st_transform("+proj=eqearth +wktext")
+
+dam_glcp_link <- dam_data %>%
+  cbind(slope_data_shrink[st_nearest_feature(dam_data, slope_data_shrink),]) %>%
+  mutate(dist = st_distance(geometry, geometry.1, by_element = T))
+#
+reservoirs <- c(unique(dam_glcp_link$hylak_id))
+
+`%!in%` <- Negate(`%in%`)
+
+slope_data_shrink_2 <- slope_data_shrink %>%
+  filter(hylak_id %!in% reservoirs)
+
+slope_data_shrink_2 <- slope_data_shrink_2[!duplicated(slope_data_shrink_2$hylak_id), ]
+
+# takes a while! Sit tight. 
+link <- slope_data_shrink_2 %>%
+  cbind(EcoregionMask_hex[st_nearest_feature(slope_data_shrink_2, EcoregionMask_hex),]) %>%
+  mutate(dist = st_distance(geometry, geometry.1, by_element = T))
+
+lakes_boreal_shrink <- link %>%
+  mutate(biome_type = case_when(
+    BIOME == 1 ~ "TROPICAL MOIST FOREST",
+    BIOME == 2 ~ "TROPICAL DRY FOREST",
+    BIOME == 3 ~ "TROPICAL CONIFEROUS FOREST",
+    BIOME == 4 ~ "TEMPERATE BROADLEAF FOREST",
+    BIOME == 5 ~ "TEMPERATE CONIFEROUS FOREST",
+    BIOME == 6 ~ "BOREAL FOREST",
+    BIOME == 7 ~ "TROPICAL GRASSLAND",
+    BIOME == 8 ~ "TEMPERATE GRASSLAND",
+    BIOME == 9 ~ "FLOODED GRASSLAND",
+    BIOME == 10 ~ "MONTANE GRASSLAND",
+    BIOME == 11 ~ "TUNDRA",
+    BIOME == 12 ~ "MEDITERRANIAN FOREST",
+    BIOME == 13 ~ "DESERT",
+    BIOME == 14 ~ "MANGROVES",
+    BIOME == 98 ~ "LAKE",
+    BIOME == 99 ~ "ROCK & ICE",
+    TRUE ~ NA_character_))%>%
+  st_transform("+proj=eqearth +wktext") %>%
+  filter(biome_type %in% c("BOREAL FOREST","ROCK & ICE","TUNDRA"))
+
+
+lakes_boreal_shrink <- c(unique(lakes_boreal_shrink$hylak_id))
+
+shp_desert_shrink <- link %>%
+  mutate(biome_type = case_when(
+    BIOME == 1 ~ "TROPICAL MOIST FOREST",
+    BIOME == 2 ~ "TROPICAL DRY FOREST",
+    BIOME == 3 ~ "TROPICAL CONIFEROUS FOREST",
+    BIOME == 4 ~ "TEMPERATE BROADLEAF FOREST",
+    BIOME == 5 ~ "TEMPERATE CONIFEROUS FOREST",
+    BIOME == 6 ~ "BOREAL FOREST",
+    BIOME == 7 ~ "TROPICAL GRASSLAND",
+    BIOME == 8 ~ "TEMPERATE GRASSLAND",
+    BIOME == 9 ~ "FLOODED GRASSLAND",
+    BIOME == 10 ~ "MONTANE GRASSLAND",
+    BIOME == 11 ~ "TUNDRA",
+    BIOME == 12 ~ "MEDITERRANIAN FOREST",
+    BIOME == 13 ~ "DESERT",
+    BIOME == 14 ~ "MANGROVES",
+    BIOME == 98 ~ "LAKE",
+    BIOME == 99 ~ "ROCK & ICE",
+    TRUE ~ NA_character_))%>%
+  st_transform("+proj=eqearth +wktext") %>%
+  filter(biome_type == "DESERT")
+
+shp_desert_shrink <- c(unique(shp_desert_shrink$hylak_id))
+
+shp_temperate_shrink <- link %>%
+  mutate(biome_type = case_when(
+    BIOME == 1 ~ "TROPICAL MOIST FOREST",
+    BIOME == 2 ~ "TROPICAL DRY FOREST",
+    BIOME == 3 ~ "TROPICAL CONIFEROUS FOREST",
+    BIOME == 4 ~ "TEMPERATE BROADLEAF FOREST",
+    BIOME == 5 ~ "TEMPERATE CONIFEROUS FOREST",
+    BIOME == 6 ~ "BOREAL FOREST",
+    BIOME == 7 ~ "TROPICAL GRASSLAND",
+    BIOME == 8 ~ "TEMPERATE GRASSLAND",
+    BIOME == 9 ~ "FLOODED GRASSLAND",
+    BIOME == 10 ~ "MONTANE GRASSLAND",
+    BIOME == 11 ~ "TUNDRA",
+    BIOME == 12 ~ "MEDITERRANIAN FOREST",
+    BIOME == 13 ~ "DESERT",
+    BIOME == 14 ~ "MANGROVES",
+    BIOME == 98 ~ "LAKE",
+    BIOME == 99 ~ "ROCK & ICE",
+    TRUE ~ NA_character_))%>%
+  st_transform("+proj=eqearth +wktext") %>%
+  filter(biome_type %in% c("TEMPERATE GRASSLAND","TEMPERATE CONIFEROUS FOREST","MEDITERRANIAN FOREST","FLOODED GRASSLAND",
+                           "MONTANE GRASSLAND","LAKE","TEMPERATE BROADLEAF FOREST"))
+
+shp_temperate_shrink <- c(unique(shp_temperate_shrink$hylak_id))
+
+shp_tropical_shrink <- link %>%
+  mutate(biome_type = case_when(
+    BIOME == 1 ~ "TROPICAL MOIST FOREST",
+    BIOME == 2 ~ "TROPICAL DRY FOREST",
+    BIOME == 3 ~ "TROPICAL CONIFEROUS FOREST",
+    BIOME == 4 ~ "TEMPERATE BROADLEAF FOREST",
+    BIOME == 5 ~ "TEMPERATE CONIFEROUS FOREST",
+    BIOME == 6 ~ "BOREAL FOREST",
+    BIOME == 7 ~ "TROPICAL GRASSLAND",
+    BIOME == 8 ~ "TEMPERATE GRASSLAND",
+    BIOME == 9 ~ "FLOODED GRASSLAND",
+    BIOME == 10 ~ "MONTANE GRASSLAND",
+    BIOME == 11 ~ "TUNDRA",
+    BIOME == 12 ~ "MEDITERRANIAN FOREST",
+    BIOME == 13 ~ "DESERT",
+    BIOME == 14 ~ "MANGROVES",
+    BIOME == 98 ~ "LAKE",
+    BIOME == 99 ~ "ROCK & ICE",
+    TRUE ~ NA_character_))%>%
+  st_transform("+proj=eqearth +wktext") %>%
+  filter(biome_type %in% c("TROPICAL MOIST FOREST","TROPICAL DRY FOREST","TROPICAL GRASSLAND","MANGROVES",
+                           "TROPICAL CONIFEROUS FOREST"))
+
+shp_tropical_shrink <- c(unique(shp_tropical_shrink$hylak_id))
