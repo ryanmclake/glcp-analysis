278fb7165914e0e356be2782467e914e1f66e521
Alli N Cramer <allison.cramer@wsu.edu>
2022-09-27 14:30:51 -0700

    Added comments
    comments added

--- code/kamiak/D2_glcp_slim_add_kendall_tau.R
+++ code/kamiak/D2_glcp_slim_add_kendall_tau.R
@@ -1,23 +1,47 @@
+# script calculates kendall tau on GLCP slim data set 
+  # glcp slim is the GLCP 2.0 with only the columns of interest
+  #script:
+     # selects columns 
+     # calculates kendall tau
+     # joins to original data set
+     # exports
 
+# =======================================================================
+#------------------------------------------------------------------------
+
+#### Libraries ####
 library(dplyr, warn.conflicts = FALSE)
 library(tidyr, warn.conflicts = FALSE)
 library(vroom, warn.conflicts = FALSE)
 library(broom, warn.conflicts = FALSE)
 library(Kendall, warn.conflicts = FALSE)
 
+#### Bringing in 'glcp slim' data set ####
 d <- vroom::vroom("./output/glcp_slim_yearly_median.csv")
 
-k <- d %>% select(hylak_id, permanent_km2) %>%
+#### Calculating kendall tau for each lake ####
+
+k <- d %>% 
+#selecting columns
+select(hylak_id, permanent_km2) %>%
+#grouping by lake id
   group_by(hylak_id) %>%
+#calculating kendall tau
   summarise(across(c(1),  ~list(MannKendall(.) %>%
                                   tidy %>%
                                   select(p.value, statistic)))) %>%
+#unnest so p.value and kendall tau statistic are separate columns
   ungroup(.) %>%
   unnest(c(2), names_repair = "minimal") %>%
+#rename 'statistic' to 'kendall_tau'
   rename(kendall_tau = statistic) %>%
+#remove 'p.value' column
   select(-p.value)
 
+#### Joining and exporting ####
+#join
 left_join(d, k, by = "hylak_id") %>%
+#export
   write.table(., file = paste0("./output/D2_glcp_slim_add_kendall_tau.csv"),
               append = T,
               row.names = F,
