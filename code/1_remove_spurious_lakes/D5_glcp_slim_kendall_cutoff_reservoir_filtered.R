0396cc30120bad36798b69ee87a27c8ea425a0b7
Alli N Cramer <allison.cramer@wsu.edu>
2022-09-29 16:20:15 -0700

    Annotated full script

--- code/1_remove_spurious_lakes/D5_glcp_slim_kendall_cutoff_reservoir_filtered.R
+++ code/1_remove_spurious_lakes/D5_glcp_slim_kendall_cutoff_reservoir_filtered.R
@@ -1,10 +1,29 @@
+
+
+# script removes lakes based on their kendall tau, area cutoff, and reservoir tag from the GCLP slim kendall cutoff reservoir data set
+  # glcp slim kendall cutoff reservoir is the GLCP 2.0 with only the columns of interest, plus the column of kendal tau of permanent water, cutoff, and reservoir tag
+  #script:
+     # imports data
+     # selects lakes with Kendall Tau's of less than 1 
+     # selects lakes above the lake area/basin area cutoff
+     # exports
+
+# =======================================================================
+#------------------------------------------------------------------------
+
 #### initial time for script start #### 
 s = Sys.time()
 
+#### Bringing in and filtering data ####
+#import
 vroom::vroom("./output/D4_glcp_slim_kendall_cutoff_add_reservoir.csv") %>%
+#select lakes with kendall tau < 1
   filter(kendall_tau < 1) %>%
+#select lakes above area cutoff
   filter(area_cutoff == "KEEP") %>%
+#select lakes tagged as LAKE
   filter(water_body_type == "LAKE") %>%
+#exports
   write.table(., file = paste0("./output/D5_glcp_slim_kendall_cutoff_reservoir_filtered.csv"),
               append = T,
               row.names = F,
