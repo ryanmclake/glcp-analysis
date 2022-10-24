72b67424337383da73f028f8baa63ca3035cfe51
Ryan McClure <rmcclure@vpn-251-20.caltech.edu>
2022-10-24 17:44:07 -0600

    more code piddling

--- code/3_pair_to_wwf_biomes/A4_glcp_filtered_get_biome_slope_categories.R
+++ code/3_pair_to_wwf_biomes/A4_glcp_filtered_get_biome_slope_categories.R
@@ -2,39 +2,14 @@
 library(reshape2)
 library(ggplot2)
 library(dplyr)
+library(imputeTS)
 
 d <- vroom::vroom("./output/A3_glcp_filtered_biomes_joined.csv")
 
-
-swell <- d %>% filter(sens.slope > 0) %>% 
-  select(hybas_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
-  group_by(hybas_id,  centr_lat, centr_lon) %>%
-  mutate(total_km2 = scale(total_km2),
-         total_precip_mm = scale(total_precip_mm),
-         mean_annual_temp_k = scale(mean_annual_temp_k),
-         pop_sum = scale(pop_sum)) %>%
-  do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
-  mutate(res_std = summary(mod)$sigma,
-         intercept = summary(mod)$coefficients[1],
-         p.val.intercept = summary(mod)$coefficients["(Intercept)", "Pr(>|t|)"],
-         precip = summary(mod)$coefficients[2],
-         p.val.precip = summary(mod)$coefficients["total_precip_mm", "Pr(>|t|)"],
-         temp = summary(mod)$coefficients[3],
-         p.val.temp = summary(mod)$coefficients["mean_annual_temp_k", "Pr(>|t|)"],
-         pop = summary(mod)$coefficients[4],
-         p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
-         r_sqr = summary(mod)$adj.r.squared)%>%
-  select(-mod, -hybas_id, -centr_lat, -centr_lon) %>%
-  ungroup(.) %>%
-  summarize_all(funs(median), na.rm = T)%>%
-  mutate(biome = "Swelling Desert Lakes")
-
-
-
-
 swell_desert <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "DESERT") %>%
-  select(hybas_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
-  group_by(hybas_id,  centr_lat, centr_lon) %>%
+  select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
+  group_by(hylak_id,  centr_lat, centr_lon) %>%
+  dplyr::mutate_at(vars(pop_sum),funs(imputeTS::na_interpolation(., option = "linear"))) %>%
   mutate(total_km2 = scale(total_km2),
          total_precip_mm = scale(total_precip_mm),
          mean_annual_temp_k = scale(mean_annual_temp_k),
@@ -42,27 +17,20 @@ swell_desert <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "DESERT") 
   do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
   mutate(res_std = summary(mod)$sigma,
          intercept = summary(mod)$coefficients[1],
-         p.val.intercept = summary(mod)$coefficients["(Intercept)", "Pr(>|t|)"],
          precip = summary(mod)$coefficients[2],
-         p.val.precip = summary(mod)$coefficients["total_precip_mm", "Pr(>|t|)"],
          temp = summary(mod)$coefficients[3],
-         p.val.temp = summary(mod)$coefficients["mean_annual_temp_k", "Pr(>|t|)"],
          pop = summary(mod)$coefficients[4],
-         p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
          r_sqr = summary(mod)$adj.r.squared)%>%
-  select(-mod, -hybas_id, -centr_lat, -centr_lon) %>%
-  ungroup(.) %>%
-  summarize_all(funs(median), na.rm = T)%>%
-  mutate(biome = "Swelling Desert Lakes")
-
-
+  mutate(biome = "Swelling Desert Lakes") %>%
+  select(-mod)
 
 
 
 
 shrink_desert <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "DESERT") %>%
-  select(hybas_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
-  group_by(hybas_id,  centr_lat, centr_lon) %>%
+  select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
+  group_by(hylak_id,  centr_lat, centr_lon) %>%
+  dplyr::mutate_at(vars(pop_sum),funs(imputeTS::na_interpolation(., option = "linear"))) %>%
   mutate(total_km2 = scale(total_km2),
          total_precip_mm = scale(total_precip_mm),
          mean_annual_temp_k = scale(mean_annual_temp_k),
@@ -70,23 +38,18 @@ shrink_desert <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "DESERT")
   do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
   mutate(res_std = summary(mod)$sigma,
          intercept = summary(mod)$coefficients[1],
-         p.val.intercept = summary(mod)$coefficients["(Intercept)", "Pr(>|t|)"],
          precip = summary(mod)$coefficients[2],
-         p.val.precip = summary(mod)$coefficients["total_precip_mm", "Pr(>|t|)"],
          temp = summary(mod)$coefficients[3],
-         p.val.temp = summary(mod)$coefficients["mean_annual_temp_k", "Pr(>|t|)"],
          pop = summary(mod)$coefficients[4],
-         p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
          r_sqr = summary(mod)$adj.r.squared)%>%
-  select(-mod,-hybas_id, -centr_lat, -centr_lon) %>%
-  ungroup(.) %>%
-  summarize_all(funs(median), na.rm = T)%>%
-  mutate(biome = "Shrinking Desert Lakes")
+  mutate(biome = "Shrinking Desert Lakes") %>%
+  select(-mod)
 
 
 swell_tropical <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "TROPICAL") %>%
-  select(hybas_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
-  group_by(hybas_id,  centr_lat, centr_lon) %>%
+  select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
+  group_by(hylak_id,  centr_lat, centr_lon) %>%
+  dplyr::mutate_at(vars(pop_sum),funs(imputeTS::na_interpolation(., option = "linear"))) %>%
   mutate(total_km2 = scale(total_km2),
          total_precip_mm = scale(total_precip_mm),
          mean_annual_temp_k = scale(mean_annual_temp_k),
@@ -94,22 +57,17 @@ swell_tropical <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "TROPICA
   do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
   mutate(res_std = summary(mod)$sigma,
          intercept = summary(mod)$coefficients[1],
-         p.val.intercept = summary(mod)$coefficients["(Intercept)", "Pr(>|t|)"],
          precip = summary(mod)$coefficients[2],
-         p.val.precip = summary(mod)$coefficients["total_precip_mm", "Pr(>|t|)"],
          temp = summary(mod)$coefficients[3],
-         p.val.temp = summary(mod)$coefficients["mean_annual_temp_k", "Pr(>|t|)"],
          pop = summary(mod)$coefficients[4],
-         p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
-         r_sqr = summary(mod)$adj.r.squared)%>%
-  select(-mod,-hybas_id, -centr_lat, -centr_lon) %>%
-  ungroup(.) %>%
-  summarize_all(funs(median), na.rm = T)%>%
+         r_sqr = summary(mod)$adj.r.squared) %>%
+  select(-mod) %>%
   mutate(biome = "Swelling Tropical Lakes")
 
 shrink_tropical <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "TROPICAL") %>%
-  select(hybas_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
-  group_by(hybas_id,  centr_lat, centr_lon) %>%
+  select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
+  group_by(hylak_id,  centr_lat, centr_lon) %>%
+  dplyr::mutate_at(vars(pop_sum),funs(imputeTS::na_interpolation(., option = "linear"))) %>%
   mutate(total_km2 = scale(total_km2),
          total_precip_mm = scale(total_precip_mm),
          mean_annual_temp_k = scale(mean_annual_temp_k),
@@ -117,68 +75,55 @@ shrink_tropical <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "TROPIC
   do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
   mutate(res_std = summary(mod)$sigma,
          intercept = summary(mod)$coefficients[1],
-         p.val.intercept = summary(mod)$coefficients["(Intercept)", "Pr(>|t|)"],
          precip = summary(mod)$coefficients[2],
-         p.val.precip = summary(mod)$coefficients["total_precip_mm", "Pr(>|t|)"],
          temp = summary(mod)$coefficients[3],
-         p.val.temp = summary(mod)$coefficients["mean_annual_temp_k", "Pr(>|t|)"],
          pop = summary(mod)$coefficients[4],
-         p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
-         r_sqr = summary(mod)$adj.r.squared)%>%
-  select(-mod,-hybas_id, -centr_lat, -centr_lon) %>%
-  ungroup(.) %>%
-  summarize_all(funs(median), na.rm = T) %>%
+         r_sqr = summary(mod)$adj.r.squared) %>%
+  select(-mod) %>%
   mutate(biome = "Shrinking Tropical Lakes")
 
 swell_temperate <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "TEMPERATE") %>%
-  select(hybas_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
-  group_by(hybas_id,  centr_lat, centr_lon) %>%
+  select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
+  group_by(hylak_id,  centr_lat, centr_lon) %>%
+  dplyr::mutate_at(vars(pop_sum),funs(imputeTS::na_interpolation(., option = "linear"))) %>%
   mutate(total_km2 = scale(total_km2),
          total_precip_mm = scale(total_precip_mm),
          mean_annual_temp_k = scale(mean_annual_temp_k),
          pop_sum = scale(pop_sum)) %>%
+  na.omit(.) %>%
   do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
   mutate(res_std = summary(mod)$sigma,
          intercept = summary(mod)$coefficients[1],
-         p.val.intercept = summary(mod)$coefficients["(Intercept)", "Pr(>|t|)"],
          precip = summary(mod)$coefficients[2],
-         p.val.precip = summary(mod)$coefficients["total_precip_mm", "Pr(>|t|)"],
          temp = summary(mod)$coefficients[3],
-         p.val.temp = summary(mod)$coefficients["mean_annual_temp_k", "Pr(>|t|)"],
          pop = summary(mod)$coefficients[4],
-         p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
-         r_sqr = summary(mod)$adj.r.squared)%>%
-  select(-mod,-hybas_id, -centr_lat, -centr_lon) %>%
-  ungroup(.) %>%
-  summarize_all(funs(median), na.rm = T) %>%
+         r_sqr = summary(mod)$adj.r.squared) %>%
+  select(-mod) %>%
   mutate(biome = "Swelling Temperate Lakes")
 
 shrink_temperate <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "TEMPERATE") %>%
-  select(hybas_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
-  group_by(hybas_id,  centr_lat, centr_lon) %>%
+  select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
+  group_by(hylak_id,  centr_lat, centr_lon) %>%
+  dplyr::mutate_at(vars(pop_sum),funs(imputeTS::na_interpolation(., option = "linear"))) %>%
   mutate(total_km2 = scale(total_km2),
          total_precip_mm = scale(total_precip_mm),
          mean_annual_temp_k = scale(mean_annual_temp_k),
          pop_sum = scale(pop_sum)) %>%
+  na.omit(.)%>%
   do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
   mutate(res_std = summary(mod)$sigma,
          intercept = summary(mod)$coefficients[1],
-         p.val.intercept = summary(mod)$coefficients["(Intercept)", "Pr(>|t|)"],
          precip = summary(mod)$coefficients[2],
-         p.val.precip = summary(mod)$coefficients["total_precip_mm", "Pr(>|t|)"],
          temp = summary(mod)$coefficients[3],
-         p.val.temp = summary(mod)$coefficients["mean_annual_temp_k", "Pr(>|t|)"],
          pop = summary(mod)$coefficients[4],
-         p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
-         r_sqr = summary(mod)$adj.r.squared)%>%
-  select(-mod,-hybas_id, -centr_lat, -centr_lon) %>%
-  ungroup(.) %>%
-  summarize_all(funs(median), na.rm = T) %>%
+         r_sqr = summary(mod)$adj.r.squared) %>%
+  select(-mod) %>%
   mutate(biome = "Shrinking Temperate Lakes")
 
 swell_boreal <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "BOREAL/TUNDRA/ICE") %>%
-  select(hybas_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
-  group_by(hybas_id,  centr_lat, centr_lon) %>%
+  select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
+  group_by(hylak_id,  centr_lat, centr_lon) %>%
+  dplyr::mutate_at(vars(pop_sum),funs(imputeTS::na_interpolation(., option = "linear"))) %>%
   mutate(total_km2 = scale(total_km2),
          total_precip_mm = scale(total_precip_mm),
          mean_annual_temp_k = scale(mean_annual_temp_k),
@@ -186,25 +131,67 @@ swell_boreal <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "BOREAL/TU
   do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
   mutate(res_std = summary(mod)$sigma,
          intercept = summary(mod)$coefficients[1],
-         p.val.intercept = summary(mod)$coefficients["(Intercept)", "Pr(>|t|)"],
          precip = summary(mod)$coefficients[2],
-         p.val.precip = summary(mod)$coefficients["total_precip_mm", "Pr(>|t|)"],
          temp = summary(mod)$coefficients[3],
-         p.val.temp = summary(mod)$coefficients["mean_annual_temp_k", "Pr(>|t|)"],
          pop = summary(mod)$coefficients[4],
-         p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
-         r_sqr = summary(mod)$adj.r.squared)%>%
-  select(-mod,-hybas_id, -centr_lat, -centr_lon) %>%
-  ungroup(.) %>%
-  summarize_all(funs(median), na.rm = T) %>%
+         r_sqr = summary(mod)$adj.r.squared) %>%
+  select(-mod) %>%
   mutate(biome = "Swelling Boreal Lakes")
 
+shrink_boreal <- d %>% filter(sens.slope <= 0) %>% filter(biome_type == "BOREAL/TUNDRA/ICE") %>%
+  select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
+  group_by(hylak_id,  centr_lat, centr_lon) %>%
+  dplyr::mutate_at(vars(pop_sum),funs(imputeTS::na_interpolation(., option = "linear"))) %>%
+  mutate(total_km2 = scale(total_km2),
+         total_precip_mm = scale(total_precip_mm),
+         mean_annual_temp_k = scale(mean_annual_temp_k),
+         pop_sum = scale(pop_sum)) %>%
+  do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
+  mutate(res_std = summary(mod)$sigma,
+         intercept = summary(mod)$coefficients[1],
+         precip = summary(mod)$coefficients[2],
+         temp = summary(mod)$coefficients[3],
+         pop = summary(mod)$coefficients[4],
+         r_sqr = summary(mod)$adj.r.squared)%>%
+  mutate(biome = "Shrinking Boreal Lakes") %>%
+  select(-mod)
+
+
+
+
+
+
+
 heat_map_coefficients <- rbind(shrink_desert, swell_desert, shrink_temperate, swell_temperate, 
-             shrink_tropical, swell_tropical, swell_boreal) %>%
+             shrink_tropical, swell_tropical, shrink_boreal, swell_boreal) %>%
   select(biome, intercept, precip, temp, pop) %>%
   rename(Intercept = intercept, Precipitation = precip, Temperature = temp, Population = pop) %>%
   melt(., id.vars = c("biome")) %>%
   rename(Biome = biome, `Coefficient Values` = value, `Coefficient` = variable)
+
+
+
+p1 <- ggplot(heat_map_coefficients, aes(y=Coefficient, x=`Coefficient Values`)) + 
+  geom_boxplot(outlier.shape = NA, aes(fill = Biome)) +
+  scale_fill_viridis_d(option = "B")+
+  coord_flip() +
+  xlim(c(-1.5,1.5))+
+  facet_wrap(~Biome, ncol = 4)+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 35,size = 12, color = "black", hjust = 1),
+        axis.text.y.left = element_text(size = 12, color = "black"),
+        legend.position = "top")
+
+
+ggsave(p1, path = ".",
+       filename = "./output/coefficient_values.jpg",
+       width = 12, height = 8, device='jpg', dpi=1000)
+
+
+
+
+
+
 
 heat_map_pvals <- rbind(shrink_desert, swell_desert, shrink_temperate, swell_temperate, 
                                shrink_tropical, swell_tropical, swell_boreal) %>%
@@ -233,21 +220,5 @@ ggplot(heat_map_pvals, aes(`Statistic`, Biome)) +
   theme(axis.text.y = element_text(angle = 30),
         legend.position = "top")
 
-# shrink_boreal <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "BOREAL/TUNDRA/ICE") %>%
-#   select(hylak_id, hybas_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
-#   group_by(hybas_id, hylak_id, centr_lat, centr_lon) %>%
-#   mutate(total_km2 = scale(total_km2),
-#          total_precip_mm = scale(total_precip_mm),
-#          mean_annual_temp_k = scale(mean_annual_temp_k),
-#          pop_sum = scale(pop_sum)) %>%
-#   do(mod = lm(total_km2 ~ total_precip_mm + mean_annual_temp_k + pop_sum, data = ., na.action = na.exclude))%>%
-#   mutate(res_std = summary(mod)$sigma,
-#          intercept = summary(mod)$coefficients[1],
-#          precip = summary(mod)$coefficients[2],
-#          p.val.precip = summary(mod)$coefficients["total_precip_mm", "Pr(>|t|)"],
-#          temp = summary(mod)$coefficients[3],
-#          p.val.temp = summary(mod)$coefficients["mean_annual_temp_k", "Pr(>|t|)"],
-#          pop = summary(mod)$coefficients[4],
-#          p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
-#          r_sqr = summary(mod)$adj.r.squared)%>%
-#   select(-mod)
+#p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
+
