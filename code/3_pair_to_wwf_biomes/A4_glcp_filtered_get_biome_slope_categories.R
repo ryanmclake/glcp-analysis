ab5e211c773b48e016c8642adcb9a7bd07dd2673
Ryan McClure <rmcclure@vpn-251-65.caltech.edu>
2022-10-25 15:13:54 -0600

    figure updates

--- code/3_pair_to_wwf_biomes/A4_glcp_filtered_get_biome_slope_categories.R
+++ code/3_pair_to_wwf_biomes/A4_glcp_filtered_get_biome_slope_categories.R
@@ -3,6 +3,7 @@ library(reshape2)
 library(ggplot2)
 library(dplyr)
 library(imputeTS)
+library(caret)
 
 d <- vroom::vroom("./output/A3_glcp_filtered_biomes_joined.csv")
 
@@ -21,11 +22,10 @@ swell_desert <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "DESERT") 
          temp = summary(mod)$coefficients[3],
          pop = summary(mod)$coefficients[4],
          r_sqr = summary(mod)$adj.r.squared)%>%
-  mutate(biome = "Swelling Desert Lakes") %>%
-  select(-mod)
-
-
-
+  select(-mod) %>%
+  ungroup(.) %>%
+  summarize_all(funs(median), na.rm = T) %>%
+  mutate(biome = "Swelling/Desert")
 
 shrink_desert <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "DESERT") %>%
   select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
@@ -42,8 +42,10 @@ shrink_desert <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "DESERT")
          temp = summary(mod)$coefficients[3],
          pop = summary(mod)$coefficients[4],
          r_sqr = summary(mod)$adj.r.squared)%>%
-  mutate(biome = "Shrinking Desert Lakes") %>%
-  select(-mod)
+  select(-mod) %>%
+  ungroup(.) %>%
+  summarize_all(funs(median), na.rm = T) %>%
+  mutate(biome = "Shrinking/Desert")
 
 
 swell_tropical <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "TROPICAL") %>%
@@ -62,7 +64,9 @@ swell_tropical <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "TROPICA
          pop = summary(mod)$coefficients[4],
          r_sqr = summary(mod)$adj.r.squared) %>%
   select(-mod) %>%
-  mutate(biome = "Swelling Tropical Lakes")
+  ungroup(.) %>%
+  summarize_all(funs(median), na.rm = T) %>%
+  mutate(biome = "Swelling/Tropical")
 
 shrink_tropical <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "TROPICAL") %>%
   select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
@@ -80,7 +84,9 @@ shrink_tropical <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "TROPIC
          pop = summary(mod)$coefficients[4],
          r_sqr = summary(mod)$adj.r.squared) %>%
   select(-mod) %>%
-  mutate(biome = "Shrinking Tropical Lakes")
+  ungroup(.) %>%
+  summarize_all(funs(median), na.rm = T) %>%
+  mutate(biome = "Shrinking/Tropical")
 
 swell_temperate <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "TEMPERATE") %>%
   select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
@@ -99,7 +105,9 @@ swell_temperate <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "TEMPER
          pop = summary(mod)$coefficients[4],
          r_sqr = summary(mod)$adj.r.squared) %>%
   select(-mod) %>%
-  mutate(biome = "Swelling Temperate Lakes")
+  ungroup(.) %>%
+  summarize_all(funs(median), na.rm = T) %>%
+  mutate(biome = "Swelling/Temperate")
 
 shrink_temperate <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "TEMPERATE") %>%
   select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
@@ -118,7 +126,9 @@ shrink_temperate <- d %>% filter(sens.slope < 0) %>% filter(biome_type == "TEMPE
          pop = summary(mod)$coefficients[4],
          r_sqr = summary(mod)$adj.r.squared) %>%
   select(-mod) %>%
-  mutate(biome = "Shrinking Temperate Lakes")
+  ungroup(.) %>%
+  summarize_all(funs(median), na.rm = T) %>%
+  mutate(biome = "Shrinking/Temperate")
 
 swell_boreal <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "BOREAL/TUNDRA/ICE") %>%
   select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
@@ -136,7 +146,9 @@ swell_boreal <- d %>% filter(sens.slope > 0) %>% filter(biome_type == "BOREAL/TU
          pop = summary(mod)$coefficients[4],
          r_sqr = summary(mod)$adj.r.squared) %>%
   select(-mod) %>%
-  mutate(biome = "Swelling Boreal Lakes")
+  ungroup(.) %>%
+  summarize_all(funs(median), na.rm = T) %>%
+  mutate(biome = "Swelling/Boreal")
 
 shrink_boreal <- d %>% filter(sens.slope <= 0) %>% filter(biome_type == "BOREAL/TUNDRA/ICE") %>%
   select(hylak_id, centr_lat, centr_lon, total_km2, total_precip_mm, mean_annual_temp_k, pop_sum) %>%
@@ -153,8 +165,10 @@ shrink_boreal <- d %>% filter(sens.slope <= 0) %>% filter(biome_type == "BOREAL/
          temp = summary(mod)$coefficients[3],
          pop = summary(mod)$coefficients[4],
          r_sqr = summary(mod)$adj.r.squared)%>%
-  mutate(biome = "Shrinking Boreal Lakes") %>%
-  select(-mod)
+  select(-mod) %>%
+  ungroup(.) %>%
+  summarize_all(funs(median), na.rm = T) %>%
+  mutate(biome = "Shrinking/Boreal")
 
 
 
@@ -171,54 +185,39 @@ heat_map_coefficients <- rbind(shrink_desert, swell_desert, shrink_temperate, sw
 
 
 
-p1 <- ggplot(heat_map_coefficients, aes(y=Coefficient, x=`Coefficient Values`)) + 
-  geom_boxplot(outlier.shape = NA, aes(fill = Biome)) +
-  scale_fill_viridis_d(option = "B")+
-  coord_flip() +
-  xlim(c(-1.5,1.5))+
-  facet_wrap(~Biome, ncol = 4)+
-  theme_classic()+
-  theme(axis.text.x = element_text(angle = 35,size = 12, color = "black", hjust = 1),
-        axis.text.y.left = element_text(size = 12, color = "black"),
-        legend.position = "top")
+# p1 <- ggplot(heat_map_coefficients, aes(y=Coefficient, x=`Coefficient Values`)) + 
+#   geom_boxplot(outlier.shape = NA, aes(fill = Biome)) +
+#   scale_fill_viridis_d(option = "B")+
+#   coord_flip() +
+#   xlim(c(-1.5,1.5))+
+#   facet_wrap(~Biome, ncol = 4)+
+#   theme_classic()+
+#   theme(axis.text.x = element_text(angle = 35,size = 12, color = "black", hjust = 1),
+#         axis.text.y.left = element_text(size = 12, color = "black"),
+#         legend.position = "top")
+# 
+# 
+# ggsave(p1, path = ".",
+#        filename = "./output/coefficient_values.jpg",
+#        width = 12, height = 8, device='jpg', dpi=1000)
 
 
-ggsave(p1, path = ".",
-       filename = "./output/coefficient_values.jpg",
-       width = 12, height = 8, device='jpg', dpi=1000)
-
-
-
-
-
-
-
-heat_map_pvals <- rbind(shrink_desert, swell_desert, shrink_temperate, swell_temperate, 
-                               shrink_tropical, swell_tropical, swell_boreal) %>%
-  select(biome, p.val.intercept, p.val.precip, p.val.temp, p.val.pop) %>%
-  rename(Intercept = p.val.intercept, Precipitation = p.val.precip, Temperature = p.val.temp, Population = p.val.pop) %>%
-  melt(., id.vars = c("biome")) %>%
-  rename(Biome = biome, `Coefficient P-value` = value, `Statistic` = variable)
-
-ggplot(heat_map_coefficients, aes(`Coefficient`, Biome)) +
+p2 <- ggplot(heat_map_coefficients, aes(`Coefficient`, Biome)) +
   geom_tile(aes(fill = `Coefficient Values`)) +
   scale_fill_gradient2(midpoint=0, low="orange1", mid="white",
                        high="cyan", space ="Lab", na.value="grey",
-                       name = "Coefficient Values")+
+                       name = "Trend/Biome median coefficient values")+
   theme_classic()+
   theme(axis.text.y = element_text(angle = 16),
         legend.position = "top", 
         axis.text.y.left = element_text(size = 16, color = "black"),
         axis.text.x = element_text(size = 16, color = "black"),
         axis.title = element_blank(),
-        legend.title = element_text(size = 16))
-  
-ggplot(heat_map_pvals, aes(`Statistic`, Biome)) +
-  geom_tile(aes(fill = `Coefficient P-value`)) +
-  scale_fill_viridis_c(option = "A")+
-  theme_classic()+
-  theme(axis.text.y = element_text(angle = 30),
-        legend.position = "top")
+        legend.title = element_text(size = 12))
+
+ggsave(p2, path = ".",
+       filename = "./output/coefficient_medians.jpg",
+       width = 12, height = 6, device='jpg', dpi=1000)
 
 #p.val.pop = summary(mod)$coefficients["pop_sum", "Pr(>|t|)"],
 
